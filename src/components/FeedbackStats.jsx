bdc34a3ed6bd3cde3ca64fa2d58479146e9c899b
Paula Oliveira <poliveira@itx.com>
2022-07-04 15:48:02 -0300

    Add context

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,6 +1,9 @@
-import PropTypes from 'prop-types'
+import { useContext } from 'react'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackStats( {feedback} ) {
+function FeedbackStats() {
+
+    const {feedback} = useContext(FeedbackContext)
 
     let avarage = feedback.reduce((accumulater, current) => {
         return accumulater + current.rating
