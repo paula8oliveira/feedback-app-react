51596d16a7f35f594d9881cc69d17d38ae5d3a25
Paula Oliveira <poliveira@itx.com>
2022-07-05 17:47:11 -0300

    Add json server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,31 +1,28 @@
 import {v4 as uuidv4} from 'uuid'
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
-    const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            text: 'This is Feedback item 1',
-            rating: 10,
-        },
-        {
-            id: 2,
-            text: 'This is Feedback item 2',
-            rating: 9,
-        },
-        {
-            id: 3,
-            text: 'This is Feedback item 3',
-            rating: 7,
-        }
-    ])
+    const[isloading, setIsLoading] = useState(true)
+    const [feedback, setFeedback] = useState([])
 
     const [feedbackEditable, setfeedbackEditable] = useState({
         item: {},
         edit: false
     })
+
+    useEffect(() => {
+        fetchFeedback()
+    }, [])
+
+    const fetchFeedback = async () => {
+        const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+        const data = await response.json()
+
+        setFeedback(data)
+        setIsLoading(false)
+    }
 
     const deleteFeedback = (id) => {
         if(window.confirm('Are you sure you want to delete?')) {
@@ -55,6 +52,7 @@ export const FeedbackProvider = ({children}) => {
         value={{
             feedback,
             feedbackEditable,
+            isloading,
             deleteFeedback,
             addFeedback,
             editFeedback,
